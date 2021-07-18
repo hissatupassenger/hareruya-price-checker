869cc7e27344e3ff0868b4e89c485e1c92aaef2b
root <root@vultr.guest>
2021-07-18 09:37:02 +0000

    save to file, multiple page

--- crawler.py
+++ crawler.py
@@ -1,9 +1,24 @@
 import requests
 from bs4 import BeautifulSoup
 
-r = requests.get("https://www.hareruyamtg.com/ja/products/search?category=1&cardset=263&page=1")
-soup = BeautifulSoup(r.text,"html.parser")
-price = soup.find_all("p", attrs={'class': 'itemDetail__price'})
-name  = [soup.find_all("div", attrs={'class': 'itemData'})[i].find("a").text for i in range(len(price))]
-for i in range(len(price)):
-    print(name[i], price[i].text)
+
+
+for ii in range(300):
+    f = open("crawl_result.dat","a")
+    r = requests.get("https://www.hareruyamtg.com/ja/products/search?category=1&cardset="+str(ii)+"&page=1")
+    soup = BeautifulSoup(r.text,"html.parser")
+    price = soup.find_all("p", attrs={'class': 'itemDetail__price'})
+    name  = [soup.find_all("div", attrs={'class': 'itemData'})[i].find("a").text for i in range(len(price))]
+    for i in range(len(price)):
+        f.write(name[i]+price[i].text)
+    numofgoods = int(soup.find("p",attrs={"class":"count_number"}).text)
+    for j in range(2,int(numofgoods//60+1)):
+        print("ii=",ii,"j=",j)
+        r = requests.get("https://www.hareruyamtg.com/ja/products/search?category=1&cardset="+str(ii)+"&page="+str(j))
+        soup = BeautifulSoup(r.text,"html.parser")
+        price = soup.find_all("p", attrs={'class': 'itemDetail__price'})
+        name  = [soup.find_all("div", attrs={'class': 'itemData'})[i].find("a").text for i in range(len(price))]
+        for i in range(len(price)):
+            f.write(name[i]+price[i].text)
+    f.close()
+
